5f5b0e1725caaf505c712a5e2fe2847d1c2158e3
Anam020990 <anamikaa@skillup.tech>
2021-07-02 13:35:24 +0000

    Adding temporary changes to Github

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,10 +5,14 @@ from django.utils.timezone import now
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null= False, max_length=30, default='Audi')
+    description = models.CharField(null= False, max_length=300, default='Audi cars are awesome')
+
+    def __str__(self):
+        return 'Name:' + self.name + ',' + \
+            'Description:' + self.description
+
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
