a80cf31bfd4928bc223fcbd12f922a5d247acd1c
Anam020990 <anamikaa@skillup.tech>
2021-08-02 17:16:23 +0000

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,22 +94,21 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    context = {}
     if request.method == "GET":
         url = "https://76079649.eu-gb.apigw.appdomain.cloud/api/dealership"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
-        # Concat all dealer's short name
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
-        # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        context["dealership_list"] = dealerships
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-def get_dealer_details(request, dealer_id):
+def get_dealer_details(request, dealerId):
     context = {}
     if request.method == "GET":
-        url = 'https://76079649.eu-gb.apigw.appdomain.cloud/api/reviews'
-        context = {"reviews":  restapis.get_dealer_reviews_by_id_from_cf(url, dealer_id)}
+        url = 'https://76079649.eu-gb.apigw.appdomain.cloud/api/review'
+        context = {"reviews":  restapis.get_dealer_reviews_by_id_from_cf(url, dealerId)}
         return render(request, 'djangoapp/dealer_details.html', context)
         return HttpResponse(dealer_id)
 
